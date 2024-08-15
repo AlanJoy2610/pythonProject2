e20d066fb850f42b44ea9233e5c282cb887ec987
alanj <alanjoykuriakose@gmail.com>
2024-08-15 13:01:45 +0530

    Day-2

--- src/ex_10082024/Lab004.py
+++ src/ex_10082024/Lab004.py
@@ -1,3 +1,6 @@
 name="Alan"
 
-print(type(name))
+age=25
+
+print(type(name),sep=",", end=',')
+print(type(age))
--- src/ex_10082024/Lab005.py
+++ src/ex_10082024/Lab005.py
@@ -6,3 +6,6 @@ sum=a+b+c
 sum=sum-5
 sum=sum+1
 print(sum)
+
+complex_num=2+3j
+print(complex_num.real)
